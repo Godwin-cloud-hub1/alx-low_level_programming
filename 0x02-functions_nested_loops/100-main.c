6183dca226f7f18dc0fc42cf6812ac75ff3b93d9
Godwin <smartgod313@gmail.com>
2023-07-19 15:44:18 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-main.c
+++ 0x02-functions_nested_loops/100-main.c
@@ -2,32 +2,19 @@
 
 /**
  * main - check the code.
- *
  * Return: Always 0.
  */
-int main(void)
-{
-    int r;
 
-    r = print_sign(98);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
+int main(void)
+
+{
+    print_times_table(3);
     _putchar('\n');
-    r = print_sign(0);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
+    print_times_table(5);
     _putchar('\n');
-    r = print_sign(0xff);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
+    print_times_table(98);
     _putchar('\n');
-    r = print_sign(-1);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
+    print_times_table(12);  
     return (0);
 }
+
