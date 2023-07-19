ef1fb84f8bc813f876b3e3415d22c2b52234917f
Godwin <smartgod313@gmail.com>
2023-07-19 14:09:43 -0700

    compiler fixes

---
+++ 0x02-functions_nested_loops/main2.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
+int print_sign(int n);
+int print_last_digit(int n);
+int add(int a, int b);
+int print_to_98(int n);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+void jack_bauer(void);
+void times_table(void);
+void print_times_table(int n);
+
+#endif /* MAIN_H */
+
